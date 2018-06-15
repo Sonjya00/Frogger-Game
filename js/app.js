4c50774e567e8fe9c82cb8c9b9175f35be7c4d36
Sonia Gorla <soniagorla@msn.com>
2018-06-14 22:20:45 -0500

    Add game over if player reaches the river

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
-// Enemies our player must avoid
+// ENEMIES
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -16,12 +12,16 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+
+//PLAYER
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -54,6 +54,13 @@ Player.prototype.handleInput = function(keypressed) {
   if (keypressed === 'down' && player.y < 405) {
     player.y += 83;
     console.log(player.y);
+  }
+  if (player.y === -10) {
+    setTimeout(function() {
+      player.x = 202;
+      player.y = 405;
+      alert('You won the game!');
+    }, 500);
   }
 };
 
